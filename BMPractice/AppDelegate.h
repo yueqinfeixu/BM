9e21de9690bcbcef118d61e7bd06e5d3e51ab4b3
J <jinfei@163.com>
2016-08-29 16:44:15 +0800

    Initial Commit

---
+++ BMPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMPractice
+//
+//  Created by mac on 16/8/29.
+//  Copyright © 2016年 iOS学习. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMPractice
+//
+//  Created by mac on 16/8/29.
+//  Copyright © 2016年 iOS学习. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
